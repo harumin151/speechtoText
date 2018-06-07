597021ba1abe87497ba9f62a57cf675f54aa011e
harumin151 <j16015@sangi.jp>
2018-06-07 12:38:22 +0900

    yes

--- src/speechtotext/MySQL.java
+++ src/speechtotext/MySQL.java
@@ -18,11 +18,11 @@ public class MySQL {
 
 	public MySQL() {
 		this.driver = "org.gjt.mm.mysql.Driver";
-        this.server = "sangi2018.sist.ac.jp";
+        this.server = "J16015";
         this.dbname = "sangi2018";
         this.url = "jdbc:mysql://" + server + "/" + dbname + "?useUnicode=true&characterEncoding=UTF-8";
         this.user = "sangi2018";
-        this.password = "sistsangi2018";
+        this.password = "J16015";
         this.id = "J16015";
 
         try {
